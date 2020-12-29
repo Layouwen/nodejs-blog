af6da135afabf3b1c4e49d47f9067d5316cc4cfa
liangyouwen <layouwen@gmail.com>
2020-12-30 00:19:59 +0800

    安装 cross-env nodemon 依赖

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const serverHandle = (req, res) => {
 
   const resData = {
     name: '梁又文',
-    site: '我是标题'
+    site: '我是标题',
+    env: process.env.NODE_ENV
   }
 
   res.end(JSON.stringify(resData))
