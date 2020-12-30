9dddf9b5d67b8cf6bb2e839df7b3f6b421112846
layouwen <layouwen@gmail.com>
2020-12-30 13:50:34 +0800

    添加 登录接口 API

---
+++ src/controller/user.js
@@ -0,0 +1,5 @@
+const loginCheck = (username, password) => {
+  return username === 'layouwen' && password === '123456'
+}
+
+module.exports = { loginCheck }
--- src/router/user.js
+++ src/router/user.js
@@ -1,10 +1,17 @@
+const { ErrorModel, SuccessModel } = require('../model/resModel')
+const { loginCheck } = require('../controller/user')
+
 const handleUserRouter = (req, res) => {
   const method = req.method
 
-  if (method === '/POST' && req.path === '/api/user/login') {
-    return {
-      msg: '登录成功'
-    }
+  // 解构 用户名 和 密码
+  const { username, password } = req.body
+
+  // 登录
+  if (method === 'POST' && req.path === '/api/user/login') {
+    const result = loginCheck(username, password)
+    if (result === true) return new SuccessModel()
+    return new ErrorModel('登录失败')
   }
 }
 
