a6fa49082e567614e747c3537c96c83656b4f5d2
layouwen <layouwen@gmail.com>
2021-01-14 16:05:08 +0800

    koa2 use morgan

--- blog-koa2/app.js
+++ blog-koa2/app.js
@@ -7,6 +7,9 @@ const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const session = require('koa-generic-session')
 const redisStore = require('koa-redis')
+const path = require('path')
+const fs = require('fs')
+const morgan = require('koa-morgan')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -41,6 +44,25 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+
+// 日记记录
+
+const ENV = process.env.NODE_ENV
+if (ENV !== 'production') {
+  // 开发环境
+  app.use(morgan('dev'))
+} else {
+  // 线上环境
+  const logFileName = path.join(__dirname, 'logs', 'access.log')
+  const writeStream = fs.createWriteStream(logFileName, {
+    flags: 'a',
+  })
+  app.use(
+    morgan('combined', {
+      stream: writeStream,
+    })
+  )
+}
 
 // session 配置
 app.keys = ['D@N#13sdf_']
